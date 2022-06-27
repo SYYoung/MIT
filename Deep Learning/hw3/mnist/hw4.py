772edc77b28617c361fcb9791b1140dbc3b416d2
SY Young <r38411@yahoo.com>
2022-06-27 12:11:19 +0800

    working on HW4-q7_4

--- Deep Learning/hw3/mnist/hw4.py
+++ Deep Learning/hw3/mnist/hw4.py
@@ -194,6 +194,92 @@ def svm_obj(x, y, th, th0, lam):
     val = np.mean(Lh) + lam * norm_sq
     return val
 
+# Returns the gradient of hinge(v) with respect to v.
+def d_hinge(v):
+    val = np.where(v < 1, -1, 0)
+    return val
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th
+def d_hinge_loss_th(x, y, th, th0):
+    margin = y * ((th.T @ x) + th0)
+    loss = d_hinge(margin)
+    loss = loss * y * x
+    return loss
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th0
+def d_hinge_loss_th0(x, y, th, th0):
+    margin = y * ((th.T @ x) + th0)
+    loss = d_hinge(margin)
+    loss = loss * y
+    return loss
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th
+def d_svm_obj_th(x, y, th, th0, lam):
+    dLh_dth = d_hinge_loss_th(x, y, th, th0)
+    val = np.mean(dLh_dth) + 2 * lam * th
+    return val
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th0
+def d_svm_obj_th0(x, y, th, th0, lam):
+    dLh_dt0 = d_hinge_loss_th0(x, y, th, th0)
+    val = np.mean(dLh_dt0)
+    return val
+
+# Returns the full gradient as a single vector (which includes both th, th0)
+def svm_obj_grad(X, y, th, th0, lam):
+    dsvm_dth = d_svm_obj_th(X, y, th, th0, lam)
+    dsvm_dt0 = d_svm_obj_th0(X, y, th, th0, lam)
+    return np.vstack((dsvm_dth, dsvm_dt0))
+
+def batch_svm_min(data, labels, lam):
+    def svm_min_step_size_fn(i):
+       return 2/(i+1)**0.5
+    pass
+
+
+def q7_2():
+    X1 = np.array([[1, 2, 3, 9, 10]])
+    y1 = np.array([[1, 1, 1, -1, -1]])
+    th1, th10 = np.array([[-0.31202807]]), np.array([[1.834]])
+    X2 = np.array([[2, 3, 9, 12],
+                   [5, 2, 6, 5]])
+    y2 = np.array([[1, -1, 1, -1]])
+    th2, th20 = np.array([[-3., 15.]]).T, np.array([[2.]])
+
+    d_hinge(np.array([[71.]])).tolist()
+    d_hinge(np.array([[-23.]])).tolist()
+    d_hinge(np.array([[71, -23.]])).tolist()
+
+    d_hinge_loss_th(X2[:, 0:1], y2[:, 0:1], th2, th20).tolist()
+    d_hinge_loss_th(X2, y2, th2, th20).tolist()
+    d_hinge_loss_th0(X2[:, 0:1], y2[:, 0:1], th2, th20).tolist()
+    d_hinge_loss_th0(X2, y2, th2, th20).tolist()
+
+    d_svm_obj_th(X2[:, 0:1], y2[:, 0:1], th2, th20, 0.01).tolist()
+    d_svm_obj_th(X2, y2, th2, th20, 0.01).tolist()
+    d_svm_obj_th0(X2[:, 0:1], y2[:, 0:1], th2, th20, 0.01).tolist()
+    d_svm_obj_th0(X2, y2, th2, th20, 0.01).tolist()
+
+    ans = svm_obj_grad(X2, y2, th2, th20, 0.01).tolist()
+    print(ans)
+    ans = svm_obj_grad(X2[:, 0:1], y2[:, 0:1], th2, th20, 0.01).tolist()
+    print(ans)
+
+def separable_medium():
+    X = np.array([[2, -1, 1, 1],
+                  [-2, 2, 2, -1]])
+    y = np.array([[1, -1, 1, -1]])
+    return X, y
+
+def q7_3():
+    sep_m_separator = np.array([[ 2.69231855], [ 0.67624906]]), np.array([[-3.02402521]])
+
+    x_1, y_1 = super_simple_separable()
+    ans = package_ans(batch_svm_min(x_1, y_1, 0.0001))
+
+    x_1, y_1 = separable_medium()
+    ans = package_ans(batch_svm_min(x_1, y_1, 0.0001))
+
 def q7_1():
     sep_e_separator = np.array([[-0.40338351], [1.1849563]]), np.array([[-2.26910091]])
 
@@ -226,9 +312,16 @@ def q6_3():
     ans = package_ans(minimize(f2, cv([0., 0.]), lambda i: 0.01, 1000))
     print(ans)
 
+print("q6_3()")
 q6_3()
+print("q1()")
 q1()
+print("q3()")
 q3()
+print("q6_2()")
 q6_2()
+print("q7_1()")
 q7_1()
+print("q7_2()")
+q7_2()
 
